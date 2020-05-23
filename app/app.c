796f4f946a7aecb8290da3136aa66e98fef9fdb0
cozyBoi <kjjh1126@sogang.ac.kr>
2020-05-24 01:49:51 +0900

    add debug ioctl

--- app/app.c
+++ app/app.c
@@ -7,7 +7,7 @@ Auth : largest@huins.com */
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/ioctl.h>
+#include <asm/ioctl.h>
 #include <fcntl.h>
 
 #include <string.h>
