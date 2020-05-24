590baab223b10039f8813ae4c9405ecc0fe00f95
cozyBoi <kjjh1126@sogang.ac.kr>
2020-05-24 15:31:27 +0900

    final

--- driver/driver.c
+++ driver/driver.c
@@ -227,6 +227,7 @@ static void kernel_timer_blink(unsigned long timeout) {
     kernel_call_cnt++; //count calling
     p_data->count--;
     if( p_data->count < 0 ) {
+        int i;
         loc[3] = loc[2] = loc[1] = loc[0] = 0;
         fnd_write(loc);
         dot_write(fpga_set_blank);
